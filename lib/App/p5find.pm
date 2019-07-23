132f8a734a3de241f587849ed11385ef09b55386
Kang-min Liu <gugod@gugod.org>
2019-07-23 23:43:54 +0900

    Add a function: p5_doc_iterator

--- lib/App/p5find.pm
+++ lib/App/p5find.pm
@@ -3,8 +3,11 @@ use v5.18;
 our $VERSION = "0.01";
 
 use File::Next;
+use PPI::Document::File;
+
 use Exporter 'import';
-our @EXPORT_OK = qw(p5_source_file_iterator);
+our @EXPORT_OK = qw( p5_doc_iterator
+                     p5_source_file_iterator);
 
 my %EXCLUDED = (
     '.git' => 1,
@@ -12,6 +15,18 @@ my %EXCLUDED = (
     'CVS'  => 1,
     'node_modules' => 1,
 );
+
+sub p5_doc_iterator {
+    my ($dir) = @_;
+    my $files = p5_source_file_iterator($dir);
+    return sub {
+        my $f = $files->();
+        return undef unless defined($f);
+        my $dom = PPI::Document::File->new( $f, readonly => 1 );
+        $dom->index_locations;
+        return $dom;
+    };
+}
 
 sub p5_source_file_iterator {
     my ($dir) = @_;
@@ -21,7 +36,7 @@ sub p5_source_file_iterator {
     );
     return sub {
         my $f;
-        do { $f = $files->() } while $f && ! is_perl5_source_file($f);
+        do { $f = $files->() } while defined($f) && ! is_perl5_source_file($f);
         return $f;
     }
 }
